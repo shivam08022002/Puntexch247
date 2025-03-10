692dcb7f2a065b4fbd91a30210150f9d145b5bc2
shivam08022002 <shivam2020cs147@abesit.edu.in>
2025-03-10 14:17:42 +0530

    Added lottery card bg image

--- src/components/LotteryCard.js
+++ src/components/LotteryCard.js
@@ -18,7 +18,7 @@ const LotteryCard = ({ openLoginModal, route, isLoggedIn }) => {
       <div className="lottery-card">
         <div className="lottery-content">
           <div className="lottery-title">LOTTERY</div>
-          <div className="lottery-text">ENTER AND WIN</div>
+          {/* <div className="lottery-text">ENTER AND WIN</div> */}
           <div className="lottery-prize2">JACKPOT PRIZE</div>
           <div className="lottery-amount">₹1,00,000</div>
           <button className="lottery-enter-button" onClick={handleClick}>
